8506e4e7f84d02be84e14d40463afbc413cad7ad
KIETTASE61469\Mr Kiet <kiettase61469@fpt.edu.vn>
2017-06-15 23:12:46 +0700

    [Web][Backend] Create Model, Service of SlotHour

---
+++ Web/ChurchSystem/src/main/java/com/churchsystem/model/interfaces/SlotHourModelInterface.java
@@ -0,0 +1,12 @@
+package com.churchsystem.model.interfaces;
+
+import com.churchsystem.entity.SlothourEntity;
+
+import java.util.List;
+
+/**
+ * Created by Mr Kiet on 06/15/17.
+ */
+public interface SlotHourModelInterface {
+    List<SlothourEntity> getAllSlotHour();
+}
---
+++ Web/ChurchSystem/src/main/java/com/churchsystem/service/interfaces/SlotHourServiceInterface.java
@@ -0,0 +1,12 @@
+package com.churchsystem.service.interfaces;
+
+import com.churchsystem.entity.SlothourEntity;
+
+import java.util.List;
+
+/**
+ * Created by Mr Kiet on 06/15/17.
+ */
+public interface SlotHourServiceInterface {
+    List<SlothourEntity> getAllSlotHour();
+}
